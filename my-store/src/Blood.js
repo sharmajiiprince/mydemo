eee45a406fe801fe5f1ef5ec8719cb1149a578ce
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-03-12 22:31:40 +0530

    complete

--- my-store/src/Blood.js
+++ my-store/src/Blood.js
@@ -1,64 +1,64 @@
 import Header from "./Header";
 import React, { useState, useEffect } from "react";
 import './App.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Blood() {
-  const [group, setGroup] = useState('');
-  const [type, setType] = useState('');
-  const [result, setResult] = useState([]);
-  const change=useNavigate();
+    const [group, setGroup] = useState('');
+    const [type, setType] = useState('');
+    const [result, setResult] = useState([]);
+    const change = useNavigate();
 
-  useEffect(()=>{
-    if (!localStorage.getItem('user-name')) {
-        change('/login');
-      }
-  },[]);
+    useEffect(() => {
+        if (!localStorage.getItem('user-name')) {
+            change('/login');
+        }
+    }, []);
 
-  async function getblood() {
-    console.log('called !')
-    try {
-      const response = await fetch('https://jsonserver-iota.vercel.app/blood');
-      if (!response.ok) {
-        throw new Error('Failed to fetch user data');
-      }
-      const data = await response.json();
-      const bloodGroup = data.find(blood => Object.keys(blood)[0] === group);
-      if (type === 'donner') {
-        setResult(bloodGroup[group][0].donner);
-      }
-      if (type === 'acceptor') {
-        setResult(bloodGroup[group][1].acceptor);
-      }
-      console.log(result);
-    } catch (error) {
-      console.error('Login Error:', error);
+    async function getblood() {
+        console.log('called !')
+        try {
+            const response = await fetch('https://jsonserver-iota.vercel.app/blood');
+            if (!response.ok) {
+                throw new Error('Failed to fetch user data');
+            }
+            const data = await response.json();
+            const bloodGroup = data.find(blood => Object.keys(blood)[0] === group);
+            if (type === 'donner') {
+                setResult(bloodGroup[group][0].donner);
+            }
+            if (type === 'acceptor') {
+                setResult(bloodGroup[group][1].acceptor);
+            }
+            console.log(result);
+        } catch (error) {
+            console.error('Login Error:', error);
+        }
     }
-  }
 
-  return (
-    <div>
-      <Header />
-      <div className="blood">
-        <form>
-          <label>Enter Your Blood Group:</label>
-          <input type="text" value={group} onChange={(e) => { setGroup(e.target.value) }} /><br />
-          <label>What Is Your Type:</label>
-          <select value={type} onChange={(e) => { setType(e.target.value) }}>
-            <option value="">Select Type</option>
-            <option value="donner">Donner</option>
-            <option value="acceptor">Acceptor</option>
-          </select>
-          <button type="button" onClick={getblood}>Find</button>
-        </form>
-      </div>
-      <div className="card">
-        <div className="bloodlist">
-          {result && result.map((data, index) => (
-            <h2 key={index}>{data}</h2>
-          ))}
+    return (
+        <div>
+            <Header />
+            <div className="blood">
+                <form>
+                    <label>Enter Your Blood Group:</label>
+                    <input type="text" value={group} onChange={(e) => { setGroup(e.target.value) }} /><br />
+                    <label>What Is Your Type:</label>
+                    <select value={type} onChange={(e) => { setType(e.target.value) }}>
+                        <option value="">Select Type</option>
+                        <option value="donner">Donner</option>
+                        <option value="acceptor">Acceptor</option>
+                    </select>
+                    <button type="button" onClick={getblood}>Find</button>
+                </form>
+            </div>
+            <div className="card">
+                <div className="bloodlist">
+                    {result && result.map((data, index) => (
+                        <h2 key={index}>{data}</h2>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
